c6d92521d844896200157bd43b1fe70dc7336681
meowtec <bertonzh@gmail.com>
2023-04-15 22:15:40 +0800

    New tauri app with log plugin, and it will start successfully

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,19 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use tauri_plugin_log::LogTarget;
+
+fn main() {
+    tauri::Builder::default()
+        .plugin(
+            tauri_plugin_log::Builder::default()
+                .targets([LogTarget::LogDir, LogTarget::Stdout, LogTarget::Webview])
+                .build(),
+        )
+        .setup(move |_app| {
+            log::info!("setup app");
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
